c56bd8646d3019e0e5be8ce4243fb1320a8cb456
BurkeDesigns <77447630+BurkeDesigns@users.noreply.github.com>
2026-02-19 16:14:35 -0500

    Update createAllContent.ts

--- bun-transcribe/scripts/createAllContent.ts
+++ bun-transcribe/scripts/createAllContent.ts
@@ -2,10 +2,14 @@ import { generateTranscription, generateCudaTranscription, createNotes } from ".
 import { createBibleStudy } from "./createbiblestudy";
 import { createDevotional } from "./createdevotional";
 import { generateVideoTitles } from "./yt-videos";
+import { $ } from "bun";
 
 // let videoId = "vzGg6VnoSSw";
 // await createNotesGemini('aP5-1HF932A');
 
+// stop vllm processing
+
+// await $`pm2 stop vllm-openai-service`;
 // const missingVideos = await generateVideoTitles();
 // for (const video of missingVideos) {
 //   const videoId = video.videoId;
@@ -15,6 +19,7 @@ import { generateVideoTitles } from "./yt-videos";
 //   await createBibleStudy(videoId);
 //   await createDevotional(videoId);
 // }
+// await $`pm2 start vllm-openai-service`;
 
 
 // await generateTranscription(videoId);
